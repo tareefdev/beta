495a1baa425261d011f4ea921910811dfd6a84c9
tareef mando <tareef@fastmail.com>
2019-04-12 19:53:26 +0300

    adds new files

---
+++ src/components/mdLink.js
@@ -0,0 +1,15 @@
+import React from "react"
+import LocalizedLink from "./localizedLink"
+
+const isHash = str => /^#/.test(str)
+const isInternal = to => /^\/(?!\/)/.test(to)
+
+// Only use <LocalizedLink /> for internal links
+const MdLink = ({ href, ...props }) =>
+  isHash(href) || !isInternal(href) ? (
+    <a {...props} href={href} />
+  ) : (
+    <LocalizedLink {...props} to={href} />
+  )
+
+export default MdLink
