952e48133349377a3c206711491430c9dffff8fb
tareef mando <tareef@fastmail.com>
2019-04-13 21:16:25 +0300

    fixes location in 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,13 +4,13 @@ import { graphql } from "gatsby"
 import { Layout } from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = ({ data }) => {
+const NotFoundPage = ({ data, location }) => {
   
 // const { data } = this.props
   const siteTitle = data.site.siteMetadata.title
 
     return (
-      <Layout location={data.location} title={siteTitle}>
+      <Layout location={location} title={siteTitle}>
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
