e987f701392469da76c9779f81f049b9121aa39c
tareef mando <tareef@fastmail.com>
2019-04-19 12:08:07 +0300

    removes footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -68,11 +68,6 @@ const Layout = ({ title, children }) => {
     >
       <header>{header}</header>
       <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
     </div>
   )
 };
