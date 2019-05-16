6cad055e2bf10a314a9848786201e6ab6b9b9dfa
tareef mando <tareef@fastmail.com>
2019-05-16 11:53:42 +0300

    database page

---
+++ src/pages/database.js
@@ -0,0 +1,15 @@
+import React, { useContext, Component } from 'react';
+
+class ObservationDatabase extends Component {
+  constructor(props){
+    super(props);
+//    this.buildDatabase = this.buildDatabase.bind(this);
+  }
+  render() {
+    return <div>
+             hi
+           </div>;
+  }
+}
+
+export default ObservationDatabase;
