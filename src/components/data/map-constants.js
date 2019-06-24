69e0e4c4582cd320b4be06ea92399195ec8f8333
Whitney Purdum <wpurdum@costar.com>
2019-06-24 14:15:34 -0400

    add helper files

---
+++ src/components/data/map-constants.js
@@ -0,0 +1,32 @@
+const mapConstants = {
+    mapRatio: 0.5,
+    mapRatioAdjuster: 6,
+    syriaCenter: [38, 35],
+    margin: { top: 10, left: 10, bottom: 10, right: 2 },
+    resizeScale: 1.2,
+    mouseoutDuration: 200,
+    colorLegend: {
+        original: {
+          first: '#fcaf6d',
+          second: '#fdba80',
+          third: '#fdc494',
+          fourth: '#fdcfa7',
+          fifth: '#fed9ba',
+          sixth: '#fee4cd',
+          seventh: '#feeee1'
+        },
+        dim: {
+          first: '#a58e7a',
+          second: '#a59180',
+          third: '#a58e7a',
+          fourth: '#a5978b',
+          fifth: '#a59a91',
+          sixth: '#a59e97',
+          seventh: '#a5a19d'
+        }
+      }
+
+    
+};
+
+export  { mapConstants };
