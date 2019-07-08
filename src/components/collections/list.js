6b52f455aeca3d2a37ced15842c725e7ee40037f
tareef mando <tareef@fastmail.com>
2019-07-08 13:40:29 +0300

    using hooks for query title

--- src/components/collections/list.js
+++ src/components/collections/list.js
@@ -1,45 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
 import LocalizedLink from "../../components/localizedLink";
 import tr from '../../components/useTranslations';
 
 const ListCollection = ({allUnits, units, updateUnits, setHoveredUnit}) => {
   
-//  const [queryTitle, setQueryTitle] = useState('');
-  let queryTitle = '';
+  const [queryTitle, setQueryTitle] = useState('');
+
+  const query = {
+//    title: '',
+    dateBefore: undefined,
+    dateAfter: undefined
+  };
   
   const listItems = units.map((unit) =>
                               <div
                                 key={unit["id"]}
                                 className="unit"
-                                onMouseEnter={e => setHoveredUnit(unit)}
-                                onMouseLeave={e => setHoveredUnit({})}
+                                onMouseEnter={() => setHoveredUnit(unit)}
+                                onMouseLeave={() => setHoveredUnit({})}
                               >
 
                                 <span>{unit["incident_date_time"]}</span>
                                 <p>{unit["title"]}</p>
-                                <LocalizedLink to={`/database/units/${unit.id}`}>{tr('View')}</LocalizedLink>
+                                <LocalizedLink to={`database/units/${unit.id}`}>{tr('View')}</LocalizedLink>
                               </div>
                              );
 
-  function filter(){
+  function filterByText(){
     let updatedList = allUnits.filter(function(item){
       return item.title.toLowerCase().search(
         queryTitle.toLowerCase()) !== -1;
     });
+    if (query.dateBefore !== undefined) {
+      updatedList = updatedList.filter(function (item) {
+        const d1 = new Date(item.incident_date_time);
+        const d2 = new Date(query.dateBefore);
+        return d1 < d2;
+      });
+    }
+    if (query.dateAfter !== undefined) {
+      updatedList = updatedList.filter(function (item) {
+        let d1 = new Date(item.incident_date_time);
+        let d2 = new Date(query.dateAfter);
+        return d1 > d2;
+      });
+    }
+    console.log(queryTitle);
+    console.log(query.dateAfter);
     updateUnits(updatedList);
+    console.log(`we have ${updatedList.length} results`);
   }
 
   return (
     <div>
-      <input
-        type="text"
-        onBlur={e => queryTitle = e.target.value}
-        placeholder={'Filter'}
-      />
-      <br/>
-      <button onClick={filter}>
-        Search
-      </button>
+        <input
+          type="text"
+          onChange={e => setQueryTitle(e.target.value)}
+          placeholder={'Filter'}
+        />
+        <br/>
+        Before
+        <DayPickerInput onDayChange={day => {
+          query.dateBefore = day;
+        }} />
+        <br/>
+        After
+        <DayPickerInput onDayChange={day => {
+          query.dateAfter = day;
+        }} />
+        <br/>
+        <button onClick={filterByText}>
+          Search
+        </button>
       {listItems}
     </div>
   );
--- src/pages/chemical-collection.js
+++ src/pages/chemical-collection.js
@@ -24,7 +24,6 @@ const ChemicalCollection = ({data}) => {
   }
 
   function setHoverUnit(unit) {
-    console.log(unit.id);
     setHoveredUnit(unit);
   }
   
